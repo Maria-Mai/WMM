0e030875ecc74354372a232b353b9b5e50f12bd7
Maria-Mai <mai.blo99er@gmail.com>
2019-04-22 16:39:30 +0200

    wmm excersie one c

--- Aufgabenblatt1/trigo.R
+++ Aufgabenblatt1/trigo.R
@@ -1,8 +1,28 @@
-sinfunc <- function(w, x){
-  w**(-1) * sin(2*pi*w*x)
+sinfunc <- function(w, t, phi=0){1/w * sin(2*pi*w*t + phi)}
+color <- c("blue", "green", "red", "cyan", "orange" )
+
+layout(matrix (c(seq(1,6)), nrow=3))
+
+#a)
+curve(sinfunc(1, x), from=0, to=1, col=color[1], main="a) Sinuskurven")
+for(i in 2:5){
+  curve(sinfunc(i, x),  col=color[i], add=TRUE)
 }
 
-for(i in 1:5) {
-  curve(sinfunc(i, x), from=0, to=1, col="blue",main="Darstellung Sinuskurven", add=TRUE)
-}
+#b)
+sum_sin <- function(x){sinfunc(1,x)+sinfunc(2,x)+sinfunc(3,x)+sinfunc(4,x)+sinfunc(5,x)}
+curve(sum_sin(x), from=0, to=1, col="blue", main="b) f1 + ... + f5")
 
+#c)
+curve(sin(x))
+
+#d)
+curve(sin(x))
+
+#e)
+curve(sin(x))
+
+#f)
+curve(sin(x))
+
+layout(1)
