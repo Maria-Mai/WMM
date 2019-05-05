663af94f0450e237efb6d4f05d51871fd3a1867b
Judweep <judith.stecklina@gmx.de>
2019-05-05 22:42:03 +0200

    Upload second task

--- Aufgabenblatt2/claude.R
+++ Aufgabenblatt2/claude.R
@@ -1,9 +1,7 @@
 ## Einlesen der Daten
-funktionen = load(file = "Übung 2/claude.rda")
+funktionen = load(file = "Aufgaben/Aufgabenblatt2/claude.rda")
 wave = get(funktionen[1])
 shannon = get(funktionen[2])
-
-## als PDF zu beantworten -> was genau macht outer und sweep funktion
 
 ## b) Funktion plotten
 curve(expr = wave, from = 1, to = 2, n = 501, col = "blue")
@@ -23,4 +21,22 @@ f.seq.org = shannon(x = t.seq, times = t.seq, samples = f.seq)
 # Plotten des Originalsignals
 points(x = t.seq, y = f.seq.org, col = "red", pch = 16)
 
-## e) 
+## e) Überlagern der Abtastfunktion und der rekonstruierten Kurve
+curve(expr = wave, from = 1, to = 2, n = 501, col = "blue")
+curve(expr = shannon(x,t.seq,f.seq), from = 1, to = 2,n = 501, col ="red",add=TRUE)
+
+## f) Plot mit unterschiedlichen Abtastfrequenzen
+layout(matrix(c(1:4),ncol=2))
+freq= c(8,16,32,64)
+for (hertz in freq) {
+  thertz = seq(1,2,by = 1/hertz)
+  fhertz = wave(thertz)
+  curve(expr = wave, from = 1, to = 2, n = 501, col = "blue", ylab = "f(x)", main = paste("Sampling frequency", hertz, "Hz", sep = " "))
+  curve(expr = shannon(x,thertz,fhertz), from = 1, to = 2,n = 501, col ="red",add=TRUE)
+}
+
+layout(1)
+
+## g) Kurvenverlauf im Intervall [0,3]
+curve(expr = wave, from = 0, to = 3, n = 501, col = "blue")
+curve(expr = shannon(x,t.seq,f.seq), from = 0, to = 3,n = 501, col ="red",add=TRUE)
