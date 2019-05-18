49ae1923928984ab54a0233aaf54d76271cd990f
Maria-Mai <mai.blo99er@gmail.com>
2019-05-18 19:14:57 +0200

    finished

--- Aufgabenblatt4/moment.R
+++ Aufgabenblatt4/moment.R
@@ -1,31 +1,55 @@
-image.vec <- c(rocket.dark, rocket.icon, rocket.modes, rocket.plan,
+load("moment.rda")
+pdf("moment.pdf")
+
+images <- list(rocket.dark, rocket.icon, rocket.modes, rocket.plan,
                rocket.pyro, rocket.toy, rocket.trend, rocket.V2)
 
 #a
 moment <- function(x,plot=TRUE, ...){
   
   mom <- function(x,p,q,xs=0,ys=0){
-    sum(x*((col(x)-xs)^p) * ((row(x)-ys)^q))
+    sum(x*((row(x)-xs)^p) * ((col(x)-ys)^q))
   }
   
   center_x <- mom(x,1,0)/mom(x,0,0)
   center_y <- mom(x,0,1)/mom(x,0,0)
   
   inclination <-  1/2 * atan2(2*mom(x,1,1,center_x,center_y), mom(x,2,0, center_x, center_y)-mom(x,0,2, center_x, center_y))
-  inclinationDegree <- ifelse(inclination > 0, -(90-(inclination * 180/pi)), (90-(-inclination * 180/pi)))
+  inclinationDegree <- inclination *180/pi
+
+  if(plot==TRUE){
+    #b schwerpunkt plot 
+    plot.array(x)
+    v = center_y-1/2
+    h = nrow(x) - center_x+1/2
+    abline(h=h, v=v, col="yellow")
     
-  result <- c(center=c(center_x,center_y), inclination=inclination, inclinationDegree=inclinationDegree)
-  print(result)
-  
-  #b schwerpunkt plot 
-  plot.array(x)
-  abline(v=center_x, h=center_y)
-  
-  #c neigung plot
+    #c neigung plot
+    mtext(paste(round(inclinationDegree, digits = 2),"°"), side=4)
+    b <- tan(inclination + pi / 2)
+    a <- h - v*b
+
+    abline( a, b, col="red")
+  }
+
+  result <- c(center=c(center_x,center_y), inclinationDegree=inclinationDegree)
   
 }
 
-moment(rocket.toy)
-
 #d
+
+for(x in images){
+  layout(matrix(1:4, 2, 2, byrow=TRUE))
+  moment(x)
+  moment(1-x)
+  moment(binarise(x))
+  moment(binarise(1-x))
+  layout(1)
+}
+
+#e 
+#plot.array(rocket.modes)
+#plot.array(rocket.trend)
+
+dev.off()
 
