02c26b6b5ed65612eb6ea5b013d435e921c753e3
Judweep <judith.stecklina@gmx.de>
2019-06-11 22:53:49 +0200

    task b) completed

--- Aufgabenblatt7/naivegauss.R
+++ Aufgabenblatt7/naivegauss.R
@@ -1,29 +1,46 @@
 # a)
 naivegauss <- function(x) {
   stopifnot(is.factor(x[[ncol(x)]]))
+  
   xk <- split(x[-ncol(x)], x[, ncol(x)])
-  fac <- names(x)[ncol(x)]
+  fac <- x[[ncol(x)]]
+  c <- names(x[ncol(x)])
   
-  #p <- as.list(table(x$fac) / length(x$fac))
-  p <- as.vector(sapply (xk, nrow) / nrow(x))
+  p <- t(as.matrix(table(fac) / length(fac)))
+  #p <- as.vector(sapply (xk, nrow) / nrow(x))
   
-  #mu <- by(x[,-ncol(x)],x$fac,FUN=colMeans)
+  #mu <- by(x[,-ncol(x)],fac,FUN=colMeans)
   mu <- sapply (xk, colMeans)
   
-  #S <- by(x[,-ncol(x)],x$fac,FUN=cov)
-  S <- sapply(xk, cov)
+  #s <- by(x[,-ncol(x)],fac,FUN=cov)
+  s <- sapply(xk, cov)
   
-  structure (.Data = list(p = p,
-                          mu = mu,
-                          s = s), class = 'naivegauss')
+  structure (.Data = list(
+    p = p,
+    mu = mu,
+    s = s,
+    c = c,
+    class = 'naivegauss'
+  ))
 }
 
+# lapply über tapply um Mittelwertvektoren über alle Merkemale unterschieden durch Faktoren
+#lapply(iris[,-5],function (x) tapply(x, iris[[5]], mean))
 
 #b)
 predict.naivegauss <- function(o, newdata) {
-  print(o$mu)
-  u <- apply(newdata, 1, FUN = function(x){o$p * dnorm(x,mean = o$mu, sd = sqrt(o$S))})
-  return(u)
+
+  u <- matrix(0,nr = nrow(newdata),nc = length(o$p))
+  colnames(u) <- colnames(o$p)
+  for (i in 1:length(o$p)) {
+    u[,i] <- apply(as.matrix(newdata), 1, FUN = function(x,i) { mu <- t(o$mu[, i, drop = F]); S <- matrix(o$s[, i], nr = sqrt(length(o$s[,i]))); o$p[i] * ((1 / sqrt(det(2 * pi * S))) * exp((-1 / 2) * (x - mu) %*% solve(S) %*% t(x - mu)))}, i)
+  }
+  
+  f <- factor(apply(u, 1, which.max),levels = c(1:length(o$p)), labels = colnames(o$p))
+
+  pred <- data.frame(newdata,f )
+  colnames(pred)[ncol(pred)] <- o$c
+  return(pred)
 }
 
 # c)
