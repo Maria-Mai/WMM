da8884bc42caa2c2801c46eb8be95a66d7a2f873
Maria-Mai <mai.blo99er@gmail.com>
2019-06-29 16:18:22 +0200

    further

--- Aufgabenblatt9/unipoly.R
+++ Aufgabenblatt9/unipoly.R
@@ -8,11 +8,34 @@ polyfun <- function(x,a){
 
 #c
 polyfit <- function(xy,n){
-  lm(xy[[2]] ~ poly(xy[[1]],n, raw=TRUE))
+  if(n==0) {
+    lm(xy[[2]] ~ 1)
+  }else{
+    lm(xy[[2]] ~ poly(xy[[1]],n, raw=TRUE))
+  }
 }
 
 #d
-polyfits <- functio(xy, deg, plot=FALSE){
+polyfits <- function(xy, deg, plot=FALSE){
+  lm.list <- sapply(deg, polyfit, xy=xy, simplify = FLASE)
+  if(plot==TRUE){
+    aci.list <- sapply(lm_list, AIC)
+    bci.list <-sapply(lm_list, BIC)
+    plot()
+  }
+  lm.list
+}
+
+
+#e
+polyplot <- function(o, xy){
+
   
 }
 
+#polyfit(data, 6)
+
+#f
+polyplots <- function(xy, deg=0:11){
+  
+}
